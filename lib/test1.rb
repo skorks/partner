f3f57157d59a6c2879693ab1b236116ddb117f0e
Alan Skorkin <alan@skorks.com>
2017-03-08 23:53:02 +1100

    Add some thoughts

--- lib/test1.rb
+++ lib/test1.rb
@@ -42,37 +42,17 @@ class TokenIterator
     @current_index = 0
   end
 
-  def next(n=1)
+  def next
     if current_index == tokens.size
       nil
     else
-      tokens[current_index, n].tap do
-        @current_index += n
-      end
+      tokens[current_index]
     end
   end
 
   def has_next?
     current_index < tokens.size
   end
-
-  def look_ahead
-    n=1
-    tokens[current_index, n]
-  end
-
-  def look_behind
-    n=1
-    tokens[(current_index - n - 1)...current_index - 1]
-  end
-
-  # return all values until we see one that satisfies the block
-  def next_until(&block)
-  end
-
-  # def remaining
-  #   tokens[@current_index..tokens.size]
-  # end
 end
 
 class TokenClassifier
@@ -162,31 +142,179 @@ end
 
 require "shellwords"
 
-token_types = [
-  :long_option,
-  :option_value,
-  :noop,
-]
+# tokenization_context = TokenizationContext.new
+# token_iterator = TokenIterator.new(tokens: args1)
+# token_classifier = TokenClassifier.new(tokenization_context: tokenization_context)
 
-template = {
-  options: {
-    # foo: { short: "f", long: "foo", type: :string, default: "", required: true, desc: "Foo value" }
-    foo: { type: :string },
-    bar: { type: :boolean }
-  },
-}
-args1 = Shellwords.split("--foo")
+# while token_iterator.has_next? do
+#   token = token_iterator.next.first
+#   token_type = token_classifier.classify(token)
+#   normalized_token = token_normalizer.normalize(token, token_type)
+#   token_object = Token.new(token, token_type, normalized_token)
+#   validation_result = token_validator.validate(token_object, tokenization_context)
+#   raise validation_result.error if validation_result.failure?
+#   tokenization_context.add(token_object)
+# end
 
-tokenization_context = TokenizationContext.new
-token_iterator = TokenIterator.new(tokens: args1)
-token_classifier = TokenClassifier.new(tokenization_context: tokenization_context)
+# token_types = {
+#   terminator: "--",
+#   long_option: "--long",
+#   short_option: "-s",
+#   combined_short_options: "-spq",
+#   negated_long_option: "--no-long",
+#   option_value: {
+#     string: "abc123",
+#     comma_separated_string:"a,b,c",
+#   },
+#   long_option_with_value: "--long=hello",
+#   long_option_with_value_quoted: "--long='hello'",
+#   short_option_with_value: "-s=hello",
+#   short_option_with_value_quoted: "-s='hello'",
+#   argument_string: nil,
+#   command_string: nil,
+# }
 
-while token_iterator.has_next? do
-  token = token_iterator.next.first
-  token_type = token_classifier.classify(token)
-  normalized_token = token_normalizer.normalize(token, token_type)
-  token_object = Token.new(token, token_type, normalized_token)
-  validation_result = token_validator.validate(token_object, tokenization_context)
-  raise validation_result.error if validation_result.failure?
-  tokenization_context.add(token_object)
+# long_options - longer than 2 chars, starts with --, does not start with --no-,
+args1 = Shellwords.split(%Q(--foo 123 --cleg 'brown fox' -b --c x -spq --no-bar --baz=hello --cred="world"))
+
+
+def terminator?(token)
+  token == "--"
 end
+
+def long_option?(token)
+  token.start_with?("--") && !token.start_with?("--no-") && !terminator?(token)
+end
+
+def short_option?(token)
+  token.start_with?("-") && token.length == 2 && !terminator?(token)
+end
+
+def combined_short_options?(token)
+  token.start_with?('-') && !token.start_with?('--') && token.length > 2
+end
+
+def negated_long_option?(token)
+  token.start_with?('--no-')
+end
+
+def normalize_args(args)
+  normalized_args = []
+  args.each do |arg|
+    if combined_short_options?(arg)
+      short_options = arg[1, arg.length].split("").map{|v| "-#{v}"}
+      normalized_args += short_options
+    else
+      normalized_args += arg.split("=")
+    end
+  end
+  normalized_args
+end
+
+
+normalized_args = normalize_args(args1)
+
+class OptionUtils
+  class << self
+    def long_name_from_canonical_name(canonical_name)
+      partial_long = canonical_name.to_s.gsub("_", "-")
+      "--#{partial_long}"
+    end
+
+    def negated_long_name_from_long_name(long_name)
+      long_name.sub("--", "--no-")
+    end
+  end
+end
+
+class Option
+  class << self
+    def build(canonical_name:, type: nil, short: nil, long: nil)
+      type ||= :boolean
+      long ||= OptionUtils.long_name_from_canonical_name(canonical_name)
+      new(canonical_name, type, short, long)
+    end
+  end
+
+  attr_reader :canonical_name, :type, :short, :long
+
+  def initialize(canonical_name, type, short, long)
+    @canonical_name = canonical_name
+    @type = type
+    @short = short
+    @long = long
+  end
+
+  def flag?
+    type == :boolean
+  end
+end
+
+class Config
+  def initialize
+    @options_by_canonical_name = {}
+    @options_by_short = {}
+    @options_by_long = {}
+  end
+
+  def add_option(option)
+    @options_by_canonical_name[option.canonical_name] = option
+    @options_by_long[option.long] = option
+    @options_by_short[option.short] = option if option.short
+  end
+
+  def find_option_by_long(token)
+    @options_by_long[token]
+  end
+
+  def find_option_by_short(token)
+    @options_by_short[token]
+  end
+end
+
+config = Config.new
+config.add_option(Option.build(canonical_name: :foo, type: :string))
+config.add_option(Option.build(canonical_name: :bob, type: :boolean, short: "-b"))
+
+p normalized_args
+
+given_options = {}
+arguments = []
+next_token_position = 0
+(0...normalized_args.length).each do |index|
+  next_token_position += 1
+  token = normalized_args[index]
+  if terminator?(token)
+    break
+  elsif long_option?(token)
+    option = config.find_option_by_long(token)
+    if option
+      if option.flag?
+        given_options[option.canonical_name] = true
+      else
+        given_options[option.canonical_name] = normalized_args[index + 1]
+        # skip the next token
+      end
+      p option
+    end
+  elsif negated_long_option?(token)
+  elsif short_option?(token)
+    option = config.find_option_by_short(token)
+    if option
+      if option.flag?
+        given_options[option.canonical_name] = true
+      else
+        given_options[option.canonical_name] = normalized_args[index + 1]
+        # skip the next token
+      end
+      p option
+    end
+  end
+end
+start_position = next_token_position
+(start_position...normalized_args.length).each do |index|
+  next_token_position += 1
+  arguments << normalized_args[index]
+end
+p given_options
+p arguments
